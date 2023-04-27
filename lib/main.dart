dcfcd6114bc4224889f01669065eff59dd77df71
Ftgeorge <ftgeorgean@gmail.com>
2023-04-27 21:36:28 +0100

    Rebuilding the app with new features

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'Splashscreen/Splashscreen.dart';
+import 'package:flux/Screens/HomeScreen.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(
     MyApp(
@@ -11,7 +13,7 @@ void main() async {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MySplashScreen(),
+        home: const HomeScreen(),
         debugShowCheckedModeBanner: false,
       ),
     ),
